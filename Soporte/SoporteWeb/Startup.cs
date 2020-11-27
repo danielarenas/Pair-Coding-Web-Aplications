8e309fb11a819df7da551f6dcef84db75dcddac3
danielarenas <54421882+danielarenas@users.noreply.github.com>
2020-11-26 16:12:07 -0800

    Avance proyecto

---
+++ Soporte/SoporteWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SoporteWeb.Startup))]
+namespace SoporteWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
